12d4f62a1af558f652ae905a8bf0ff35300fa6eb
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-21 16:14:32 -0700

    9

--- opcode_2.c
+++ opcode_2.c
@@ -4,17 +4,16 @@
  * @stack: the head of the linked list in stack
  * @line_number: the line number
  */
-
 void pop(stack_t **stack, unsigned int line_number)
 {
-        stack_t *temp;
+	stack_t *temp;
 
-        if (*stack == NULL)
-        {
-                fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-                exit(EXIT_FAILURE);
-        }
-        temp = *stack;
-        *stack = (*stack)->next;
-        free(temp);
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	*stack = (*stack)->next;
+	free(temp);
 }
