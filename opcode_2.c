0764cf0a8f1f0d8253c58060d8066146c5fdfccc
sarahassan0 <sarraree5@gmail.com>
2023-06-22 02:09:02 +0300

    test

--- opcode_2.c
+++ opcode_2.c
@@ -4,6 +4,7 @@
  * @stack: the head of the linked list in stack
  * @line_number: the line number
  */
+
 void pop(stack_t **stack, unsigned int line_number)
 {
         stack_t *temp;
@@ -13,7 +14,7 @@ void pop(stack_t **stack, unsigned int line_number)
                 fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
                 exit(EXIT_FAILURE);
         }
-	temp = *stack;
-	*stack = (*stack)->next;
-	free(temp);
+        temp = *stack;
+        *stack = (*stack)->next;
+        free(temp);
 }
