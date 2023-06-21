1e379ceb551438d8c7e9316ccd57b40476eaf791
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-21 15:06:51 -0700

    8

--- opcode_2.c
+++ opcode_2.c
@@ -13,4 +13,7 @@ void pop(stack_t **stack, unsigned int line_number)
                 fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
                 exit(EXIT_FAILURE);
         }
+	temp = *stack;
+	*stack = (*stack)->next;
+	free(temp);
 }
