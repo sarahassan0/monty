5d35859b0fd7302e363992df16d6882fc465ce3a
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-21 13:42:23 -0700

    5

--- opcode_2.c
+++ opcode_2.c
@@ -1,2 +1,10 @@
 #include "monty.h"
-
+/**
+ * pop - removes the top element of the stack
+ * @stack: the head of the linked list in stack
+ * @line_number: the line number
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	
+}
