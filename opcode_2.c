589d7d37cd0c7fab64213212ca79f4c066c4a45f
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-21 15:01:35 -0700

    7

---
+++ opcode_2.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+/**
+ * pop - removes the top element of the stack
+ * @stack: the head of the linked list in stack
+ * @line_number: the line number
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+        stack_t *temp;
+
+        if (*stack == NULL)
+        {
+                fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+}
