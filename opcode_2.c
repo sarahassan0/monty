3489b6b4ede52551fd9080fa15c2921ac879849f
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-21 17:34:48 -0700

    swap

--- opcode_2.c
+++ opcode_2.c
@@ -3,7 +3,6 @@
  * pop - removes the top element of the stack
  * @stack: the head of the linked list in stack
  * @line_number: the line number
- * test
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
@@ -17,4 +16,32 @@ void pop(stack_t **stack, unsigned int line_number)
         temp = *stack;
         *stack = (*stack)->next;
         free(temp);
+}
+
+/**
+ * swap - swaps the top two elements of the stack.
+ * @stack: the head of the linked list in stack
+ * @line_number: the line number
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t temp;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+
+	if (temp->next != NULL)
+	{
+		temp->next->prev = *stack;
+		temp->prev = NULL;
+		temp->next = *stacl;
+		(*stack)->prev = temp;
+		*stack = temp;
+	}
 }
