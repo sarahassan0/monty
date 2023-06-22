fa3772d7ed17d9ed4a8a61a94c462a2ea47fa79f
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-22 10:03:55 -0700

    1

--- opcode_2.c
+++ opcode_2.c
@@ -51,7 +51,11 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-	
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fclose(interpreter->file);
+		free(interpreter->line);
+		free(interpreter);
+		exit(EXIT_FAILURE);
 	}
 }
 
