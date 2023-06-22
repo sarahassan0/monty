9292b004807c2f6dd2e2abd3099aeef831e0dd64
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-06-22 03:49:04 +0300

    Update opcode_2.c

--- opcode_2.c
+++ opcode_2.c
@@ -29,7 +29,7 @@ void swap(stack_t **stack, unsigned int line_number)
 	int tmp;
 
 	temp = *stack;
-	if (*stack == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
