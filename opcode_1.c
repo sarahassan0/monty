d3c2b2f2ec1cc420fce8254fe7c421611cee8314
sarahassan0 <sarraree5@gmail.com>
2023-06-22 08:11:34 +0300

    Update - Monty worksgcc -Wall -Werror -Wextra -pedantic   *.c -o monty

--- opcode_1.c
+++ opcode_1.c
@@ -104,6 +104,7 @@ void add(stack_t **stack, unsigned int line_number)
  * @stack: the head of the linked list in stack.
  * @line_number: the line number.
  */
+
 void nop(__attribute__((unused)) stack_t **stack, __attribute__((unused)) unsigned int line_number)
 {
 }
