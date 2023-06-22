3e1e8b4ae18591d26262506d39b796d31c1dd1da
sarahassan0 <sarraree5@gmail.com>
2023-06-22 08:11:52 +0300

    Update - Monty works

--- opcode_1.c
+++ opcode_1.c
@@ -103,6 +103,7 @@ void add(stack_t **stack, unsigned int line_number)
  * nop - doesn’t do anything.
  * @stack: the head of the linked list in stack.
  * @line_number: the line number.
+ *
  */
 
 void nop(__attribute__((unused)) stack_t **stack, __attribute__((unused)) unsigned int line_number)
