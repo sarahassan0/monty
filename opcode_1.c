79e6506fa981e40ce31052d30c92f3efa78c4f7f
sarahassan0 <sarraree5@gmail.com>
2023-06-22 05:05:31 +0300

    Update -

--- opcode_1.c
+++ opcode_1.c
@@ -100,8 +100,3 @@ void nop(stack_t **stack, unsigned int line_number)
     (void)stack;
     (void)line_number;
 }
-void nop(stack_t **stack, unsigned int line_number)
-{
-    (void)stack;
-    (void)line_number;
-}
