ac2db6d1cece0e9fb6f192c49c02313f31772ebf
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-06-22 20:20:27 +0300

    4

--- opcode_2.c
+++ opcode_2.c
@@ -61,6 +61,7 @@ void add(stack_t **stack, unsigned int line_number)
 	pop(stack, line_number);
 }
 
+
 void nop(__attribute__((unused)) stack_t **stack,
 		 __attribute__((unused)) unsigned int line_number)
 {
