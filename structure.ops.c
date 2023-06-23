cdb665fca109881a2a6a56e2f275e00ffa364fbe
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-06-23 03:54:19 +0300

    Update structure.ops.c

--- structure.ops.c
+++ structure.ops.c
@@ -29,8 +29,23 @@ void swap(stack_t **stack, unsigned int line_number)
  */
 void rotl(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
+	stack_t *temp1 = NULL;
+	stack_t *temp2 = NULL;
 	(void)line_number;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		return;
+	}
+	temp1 = (*doubly)->next;
+	temp2 = *doubly;
+	for (; temp2->next != NULL; temp2 = temp2->next)
+		;
+	temp1->prev = NULL;
+	temp2->next = *stack;
+	(*stack)->next = NULL;
+	(*stack)->prev = temp2;
+	*stack = temp1;
 }
 
 /**
